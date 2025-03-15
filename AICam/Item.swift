1e28dfe3279edb318f95cf2e08212e124d82b514
David Quiring <57224388+dvdqrng@users.noreply.github.com>
2025-03-15 16:01:09 +0100

    Initial Commit

---
+++ AICam/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AICam
+//
+//  Created by David Quiring on 15.03.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AICamTests/AICamTests.swift
@@ -0,0 +1,17 @@
+//
+//  AICamTests.swift
+//  AICamTests
+//
+//  Created by David Quiring on 15.03.25.
+//
+
+import Testing
+@testable import AICam
+
+struct AICamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
